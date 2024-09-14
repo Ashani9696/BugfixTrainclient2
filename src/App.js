f913cefdd9f190b6c9d8d64a21390667beb94457
Ashani Dilanka <ashani9996@gmail.com>
2024-09-14 17:12:39 +0530

    Remove unused useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TrainList from './components/TrainList';
 import TrainMap from './components/TrainDetail';
